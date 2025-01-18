f926c49e268fa953df63f661a268f04db2740174
Mechanical Monarchy <mechanicalMonarchyDevs@gmail.com>
2025-01-18 09:20:26 -0600

    Added Xbox Controllers to RobotContainer and moved some constants around

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import static edu.wpi.first.units.Units.*;
+
+import com.pathplanner.lib.config.PIDConstants;
+
+import edu.wpi.first.math.controller.PIDController;
+import frc.robot.generated.TunerConstants;
+
+
+/** Add your docs here. */
+public class Constants {
+
+    public static final class driveConstants {
+
+        public final static double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+        public final static double MaxAngularRate = RotationsPerSecond.of(4.5).in(RadiansPerSecond); // 4.5 rotations per second max angular velocity
+
+        // public final static double MaxSpeed = 5; //Max targeted speed in M/S (15 NORMALLY)
+        // public final static double MaxAngularRate = 1.5 * Math.PI * 3; //Max targeted rotations / second -- 3/4ths of a rotation for now
+        public final static double MaxAcceleration = 2; //Max acceleration in M/s/s;
+        public final static double MaxAngularAcceleration = 4 * Math.PI;
+        public final static double SpeedDeadbandPercentage = 0; //Deadband or Deadzone of requested speed, as a percentage of the maximum speed;
+
+        public final static double Linearity = 3; // How steep the response curve is (typically cubic, meaning 50% on stick = 12.5% speed)
+
+        public final static double ControllerDeadzone = 0.15;
+
+        public final static PIDController chassisPID = new PIDController(0.01, 0, 0);
+        public final static double ChassisPidTolerence = Math.PI/180;
+
+        public final static double ChassisModulePosX = 13.375; //Middle of robot to module left/right position
+        public final static double ChassisModulePosY = 10.375; //Middle of robot to module front/back position
+
+    }
+
+    public static final class visionConstants {
+
+        public final static double AngleDistrust = 10; // How much the angle should impact vision trust
+    
+    }
+}
